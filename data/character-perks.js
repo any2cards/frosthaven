56ea67464a19310d26d4b18868ac9e13565f18cf
any2cards <any2cards@yahoo.com>
2020-03-27 08:08:23 -0700

    Implement Character Perks

---
+++ data/character-perks.js
@@ -0,0 +1,38 @@
+[
+  {
+    "name": "banner spear perks",
+    "points": 0,
+    "image": "character-perks/banner-spear-perks.png",
+    "xws": "bannerspearperks"
+  },
+  {
+    "name": "blink blade perks",
+    "points": 1,
+    "image": "character-perks/blink-blade-perks.png",
+    "xws": "blinkbladeperks"
+  },
+  {
+    "name": "deathwalker perks",
+    "points": 2,
+    "image": "character-perks/deathwalker-perks.png",
+    "xws": "deathwalkerperks"
+  },
+  {
+    "name": "drifter perks",
+    "points": 3,
+    "image": "character-perks/drifter-perks.png",
+    "xws": "drifterperks"
+  },
+  {
+    "name": "geminate perks",
+    "points": 4,
+    "image": "character-perks/geminate-perks.png",
+    "xws": "geminateperks"
+  },
+  {
+    "name": "necromancer perks",
+    "points": 5,
+    "image": "character-perks/necromancer-perks.png",
+    "xws": "necromancerperks"
+  }
+]
