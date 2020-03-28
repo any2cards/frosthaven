a2fef43c134a3c698f252253d13197693cd2fc61
any2cards <any2cards@yahoo.com>
2020-03-28 12:52:27 -0700

    Fix Drifter & Deathwalker Attack Modifier Cards

--- data/attack-modifiers.js
+++ data/attack-modifiers.js
@@ -306,338 +306,362 @@
     "xws": "amdf16"
   },
   {
-    "name": "am-df-back",
+    "name": "am-df-17",
     "points": 51,
+    "image": "attack-modifiers/DF/am-df-17.png",
+    "xws": "amdf17"
+  },
+  {
+    "name": "am-df-18",
+    "points": 52,
+    "image": "attack-modifiers/DF/am-df-18.png",
+    "xws": "amdf18"
+  },
+  {
+    "name": "am-df-19",
+    "points": 53,
+    "image": "attack-modifiers/DF/am-df-19.png",
+    "xws": "amdf19"
+  },
+  {
+    "name": "am-df-20",
+    "points": 54,
+    "image": "attack-modifiers/DF/am-df-20.png",
+    "xws": "amdf20"
+  },
+  {
+    "name": "am-df-21",
+    "points": 55,
+    "image": "attack-modifiers/DF/am-df-21.png",
+    "xws": "amdf21"
+  },
+  {
+    "name": "am-df-22",
+    "points": 56,
+    "image": "attack-modifiers/DF/am-df-22.png",
+    "xws": "amdf22"
+  },
+  {
+    "name": "am-df-back",
+    "points": 57,
     "image": "attack-modifiers/DF/am-df-back.png",
     "xws": "amdfback"
   },
   {
     "name": "am-dw-01",
-    "points": 52,
+    "points": 58,
     "image": "attack-modifiers/DW/am-dw-01.png",
     "xws": "amdw01"
   },
   {
     "name": "am-dw-02",
-    "points": 53,
+    "points": 59,
     "image": "attack-modifiers/DW/am-dw-02.png",
     "xws": "amdw02"
   },
   {
     "name": "am-dw-03",
-    "points": 54,
+    "points": 60,
     "image": "attack-modifiers/DW/am-dw-03.png",
     "xws": "amdw03"
   },
   {
     "name": "am-dw-04",
-    "points": 55,
+    "points": 61,
     "image": "attack-modifiers/DW/am-dw-04.png",
     "xws": "amdw04"
   },
   {
     "name": "am-dw-05",
-    "points": 56,
+    "points": 62,
     "image": "attack-modifiers/DW/am-dw-05.png",
     "xws": "amdw05"
   },
   {
     "name": "am-dw-06",
-    "points": 57,
+    "points": 63,
     "image": "attack-modifiers/DW/am-dw-06.png",
     "xws": "amdw06"
   },
   {
     "name": "am-dw-07",
-    "points": 58,
+    "points": 64,
     "image": "attack-modifiers/DW/am-dw-07.png",
     "xws": "amdw07"
   },
   {
     "name": "am-dw-08",
-    "points": 59,
+    "points": 65,
     "image": "attack-modifiers/DW/am-dw-08.png",
     "xws": "amdw08"
   },
   {
     "name": "am-dw-09",
-    "points": 60,
+    "points": 66,
     "image": "attack-modifiers/DW/am-dw-09.png",
     "xws": "amdw09"
   },
   {
     "name": "am-dw-10",
-    "points": 61,
+    "points": 67,
     "image": "attack-modifiers/DW/am-dw-10.png",
     "xws": "amdw10"
   },
   {
     "name": "am-dw-11",
-    "points": 62,
+    "points": 68,
     "image": "attack-modifiers/DW/am-dw-11.png",
     "xws": "amdw11"
   },
   {
     "name": "am-dw-12",
-    "points": 63,
+    "points": 69,
     "image": "attack-modifiers/DW/am-dw-12.png",
     "xws": "amdw12"
   },
   {
     "name": "am-dw-13",
-    "points": 64,
+    "points": 70,
     "image": "attack-modifiers/DW/am-dw-13.png",
     "xws": "amdw13"
   },
   {
     "name": "am-dw-14",
-    "points": 65,
+    "points": 71,
     "image": "attack-modifiers/DW/am-dw-14.png",
     "xws": "amdw14"
   },
   {
     "name": "am-dw-15",
-    "points": 66,
+    "points": 72,
     "image": "attack-modifiers/DW/am-dw-15.png",
     "xws": "amdw15"
   },
   {
     "name": "am-dw-16",
-    "points": 67,
+    "points": 73,
     "image": "attack-modifiers/DW/am-dw-16.png",
     "xws": "amdw16"
   },
   {
     "name": "am-dw-17",
-    "points": 68,
+    "points": 74,
     "image": "attack-modifiers/DW/am-dw-17.png",
     "xws": "amdw17"
   },
   {
     "name": "am-dw-18",
-    "points": 69,
+    "points": 75,
     "image": "attack-modifiers/DW/am-dw-18.png",
     "xws": "amdw18"
   },
   {
     "name": "am-dw-19",
-    "points": 70,
+    "points": 76,
     "image": "attack-modifiers/DW/am-dw-19.png",
     "xws": "amdw19"
   },
   {
     "name": "am-dw-20",
-    "points": 71,
+    "points": 77,
     "image": "attack-modifiers/DW/am-dw-20.png",
     "xws": "amdw20"
   },
   {
-    "name": "am-dw-21",
-    "points": 72,
-    "image": "attack-modifiers/DW/am-dw-21.png",
-    "xws": "amdw21"
-  },
-  {
-    "name": "am-dw-22",
-    "points": 73,
-    "image": "attack-modifiers/DW/am-dw-22.png",
-    "xws": "amdw22"
-  },
-  {
     "name": "am-dw-back",
-    "points": 74,
+    "points": 78,
     "image": "attack-modifiers/DW/am-dw-back.png",
     "xws": "amdwback"
   },
   {
     "name": "am-ge-01",
-    "points": 75,
+    "points": 79,
     "image": "attack-modifiers/GE/am-ge-01.png",
     "xws": "amge01"
   },
   {
     "name": "am-ge-02",
-    "points": 76,
+    "points": 80,
     "image": "attack-modifiers/GE/am-ge-02.png",
     "xws": "amge02"
   },
   {
     "name": "am-ge-03",
-    "points": 77,
+    "points": 81,
     "image": "attack-modifiers/GE/am-ge-03.png",
     "xws": "amge03"
   },
   {
     "name": "am-ge-04",
-    "points": 78,
+    "points": 82,
     "image": "attack-modifiers/GE/am-ge-04.png",
     "xws": "amge04"
   },
   {
     "name": "am-ge-05",
-    "points": 79,
+    "points": 83,
     "image": "attack-modifiers/GE/am-ge-05.png",
     "xws": "amge05"
   },
   {
     "name": "am-ge-06",
-    "points": 80,
+    "points": 84,
     "image": "attack-modifiers/GE/am-ge-06.png",
     "xws": "amge06"
   },
   {
     "name": "am-ge-07",
-    "points": 81,
+    "points": 85,
     "image": "attack-modifiers/GE/am-ge-07.png",
     "xws": "amge07"
   },
   {
     "name": "am-ge-08",
-    "points": 82,
+    "points": 86,
     "image": "attack-modifiers/GE/am-ge-08.png",
     "xws": "amge08"
   },
   {
     "name": "am-ge-09",
-    "points": 83,
+    "points": 87,
     "image": "attack-modifiers/GE/am-ge-09.png",
     "xws": "amge09"
   },
   {
     "name": "am-ge-10",
-    "points": 84,
+    "points": 88,
     "image": "attack-modifiers/GE/am-ge-10.png",
     "xws": "amge10"
   },
   {
     "name": "am-ge-11",
-    "points": 85,
+    "points": 89,
     "image": "attack-modifiers/GE/am-ge-11.png",
     "xws": "amge11"
   },
   {
     "name": "am-ge-12",
-    "points": 86,
+    "points": 90,
     "image": "attack-modifiers/GE/am-ge-12.png",
     "xws": "amge12"
   },
   {
     "name": "am-ge-13",
-    "points": 87,
+    "points": 91,
     "image": "attack-modifiers/GE/am-ge-13.png",
     "xws": "amge13"
   },
   {
     "name": "am-ge-14",
-    "points": 88,
+    "points": 92,
     "image": "attack-modifiers/GE/am-ge-14.png",
     "xws": "amge14"
   },
   {
     "name": "am-ge-15",
-    "points": 89,
+    "points": 93,
     "image": "attack-modifiers/GE/am-ge-15.png",
     "xws": "amge15"
   },
   {
     "name": "am-ge-16",
-    "points": 90,
+    "points": 94,
     "image": "attack-modifiers/GE/am-ge-16.png",
     "xws": "amge16"
   },
   {
     "name": "am-ge-17",
-    "points": 91,
+    "points": 95,
     "image": "attack-modifiers/GE/am-ge-17.png",
     "xws": "amge17"
   },
   {
     "name": "am-ge-back",
-    "points": 92,
+    "points": 96,
     "image": "attack-modifiers/GE/am-ge-back.png",
     "xws": "amgeback"
   },
   {
     "name": "am-ne-01",
-    "points": 93,
+    "points": 97,
     "image": "attack-modifiers/NE/am-ne-01.png",
     "xws": "amne01"
   },
   {
     "name": "am-ne-02",
-    "points": 94,
+    "points": 98,
     "image": "attack-modifiers/NE/am-ne-02.png",
     "xws": "amne02"
   },
   {
     "name": "am-ne-03",
-    "points": 95,
+    "points": 99,
     "image": "attack-modifiers/NE/am-ne-03.png",
     "xws": "amne03"
   },
   {
     "name": "am-ne-04",
-    "points": 96,
+    "points": 100,
     "image": "attack-modifiers/NE/am-ne-04.png",
     "xws": "amne04"
   },
   {
     "name": "am-ne-05",
-    "points": 97,
+    "points": 101,
     "image": "attack-modifiers/NE/am-ne-05.png",
     "xws": "amne05"
   },
   {
     "name": "am-ne-06",
-    "points": 98,
+    "points": 102,
     "image": "attack-modifiers/NE/am-ne-06.png",
     "xws": "amne06"
   },
   {
     "name": "am-ne-07",
-    "points": 99,
+    "points": 103,
     "image": "attack-modifiers/NE/am-ne-07.png",
     "xws": "amne07"
   },
   {
     "name": "am-ne-08",
-    "points": 100,
+    "points": 104,
     "image": "attack-modifiers/NE/am-ne-08.png",
     "xws": "amne08"
   },
   {
     "name": "am-ne-09",
-    "points": 101,
+    "points": 105,
     "image": "attack-modifiers/NE/am-ne-09.png",
     "xws": "amne09"
   },
   {
     "name": "am-ne-10",
-    "points": 102,
+    "points": 106,
     "image": "attack-modifiers/NE/am-ne-10.png",
     "xws": "amne10"
   },
   {
     "name": "am-ne-11",
-    "points": 103,
+    "points": 107,
     "image": "attack-modifiers/NE/am-ne-11.png",
     "xws": "amne11"
   },
   {
     "name": "am-ne-12",
-    "points": 104,
+    "points": 108,
     "image": "attack-modifiers/NE/am-ne-12.png",
     "xws": "amne12"
   },
   {
     "name": "am-ne-13",
-    "points": 105,
+    "points": 109,
     "image": "attack-modifiers/NE/am-ne-13.png",
     "xws": "amne13"
   },
   {
     "name": "am-ne-back",
-    "points": 106,
+    "points": 110,
     "image": "attack-modifiers/NE/am-ne-back.png",
     "xws": "amneback"
   }
