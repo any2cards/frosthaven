fc4d308e79da808e1bcc72e036672da6b8efe258
any2cards <any2cards@yahoo.com>
2021-12-31 07:49:14 -0800

    Implement STD CP

--- data/character-perks.js
+++ data/character-perks.js
@@ -2,37 +2,43 @@
   {
     "name": "banner spear perks",
     "points": 0,
-    "image": "character-perks/banner-spear-perks.png",
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-banner-spear-perks.png",
     "xws": "bannerspearperks"
   },
   {
     "name": "blinkblade perks",
     "points": 1,
-    "image": "character-perks/blinkblade-perks.png",
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-blinkblade-perks.png",
     "xws": "blinkbladeperks"
   },
   {
     "name": "boneshaper perks",
     "points": 2,
-    "image": "character-perks/boneshaper-perks.png",
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-boneshaper-perks.png",
     "xws": "boneshaperperks"
   },
   {
     "name": "deathwalker perks",
     "points": 3,
-    "image": "character-perks/deathwalker-perks.png",
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-deathwalker-perks.png",
     "xws": "deathwalkerperks"
   },
   {
     "name": "drifter perks",
     "points": 4,
-    "image": "character-perks/drifter-perks.png",
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-drifter-perks.png",
     "xws": "drifterperks"
   },
   {
     "name": "geminate perks",
     "points": 5,
-    "image": "character-perks/geminate-perks.png",
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-geminate-perks.png",
     "xws": "geminateperks"
   }
 ]
