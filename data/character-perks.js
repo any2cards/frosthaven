aa179975d5c43d15da7c1bd54f45d56f7abb1057
any2cards <any2cards@yahoo.com>
2023-01-17 09:38:33 -0800

    Implement FH CP

--- data/character-perks.js
+++ data/character-perks.js
@@ -21,24 +21,178 @@
     "xws": "boneshaperperks"
   },
   {
-    "name": "deathwalker perks",
+    "name": "crashing tide perks",
     "points": 3,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-crashing-tide-perks.png",
+    "xws": "crashingtideperks"
+  },
+  {
+    "name": "coral perks",
+    "points": 4,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-crashing-tide-perks.png",
+    "xws": "crashingtideperks"
+  },
+  {
+    "name": "deathwalker perks",
+    "points": 5,
     "expansion": "Frosthaven",
     "image": "character-perks/frosthaven/fh-deathwalker-perks.png",
     "xws": "deathwalkerperks"
   },
   {
+    "name": "deepwraith perks",
+    "points": 6,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-deepwraith-perks.png",
+    "xws": "deepwraithperks"
+  },
+  {
+    "name": "kelp perks",
+    "points": 7,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-deepwraith-perks.png",
+    "xws": "deepwraithperks"
+  },
+  {
     "name": "drifter perks",
-    "points": 4,
+    "points": 8,
     "expansion": "Frosthaven",
     "image": "character-perks/frosthaven/fh-drifter-perks.png",
     "xws": "drifterperks"
   },
   {
+    "name": "frozen fist perks",
+    "points": 9,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-frozen-fist-perks.png",
+    "xws": "frozenfistperks"
+  },
+  {
+    "name": "fist perks",
+    "points": 10,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-frozen-fist-perks.png",
+    "xws": "frozenfistperks"
+  },
+  {
     "name": "geminate perks",
-    "points": 5,
+    "points": 11,
     "expansion": "Frosthaven",
     "image": "character-perks/frosthaven/fh-geminate-perks.png",
     "xws": "geminateperks"
+  },
+  {
+    "name": "hive perks",
+    "points": 12,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-hive-perks.png",
+    "xws": "hiveperks"
+  },
+  {
+    "name": "prism perks",
+    "points": 13,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-hive-perks.png",
+    "xws": "hiveperks"
+  },
+  {
+    "name": "infuser perks",
+    "points": 14,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-infuser-perks.png",
+    "xws": "infuserperks"
+  },
+  {
+    "name": "astral perks",
+    "points": 15,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-infuser-perks.png",
+    "xws": "infuserperks"
+  },
+  {
+    "name": "metal mosaic perks",
+    "points": 16,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-metal-mosaic-perks.png",
+    "xws": "metalmosaicperks"
+  },
+  {
+    "name": "drill perks",
+    "points": 17,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-metal-mosaic-perks.png",
+    "xws": "metalmosaicperks"
+  },
+  {
+    "name": "pain conduit perks",
+    "points": 18,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-pain-conduit-perks.png",
+    "xws": "painconduitperks"
+  },
+  {
+    "name": "shackles perks",
+    "points": 19,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-pain-conduit-perks.png",
+    "xws": "painconduitperks"
+  },
+  {
+    "name": "pyroclast perks",
+    "points": 20,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-pyroclast-perks.png",
+    "xws": "pyroclastperks"
+  },
+  {
+    "name": "meteor perks",
+    "points": 21,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-pyroclast-perks.png",
+    "xws": "pyroclastperks"
+  },
+  {
+    "name": "shattersong perks",
+    "points": 22,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-shattersong-perks.png",
+    "xws": "shattersongperks"
+  },
+  {
+    "name": "shards perks",
+    "points": 23,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-shattersong-perks.png",
+    "xws": "shattersongperks"
+  },
+  {
+    "name": "snowdancer perks",
+    "points": 24,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-snowdancer-perks.png",
+    "xws": "snowdancerperks"
+  },
+  {
+    "name": "snowflake perks",
+    "points": 25,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-snowdancer-perks.png",
+    "xws": "snowdancerperks"
+  },
+  {
+    "name": "trapper perks",
+    "points": 26,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-trapper-perks.png",
+    "xws": "trapperperks"
+  },
+  {
+    "name": "trap perks",
+    "points": 27,
+    "expansion": "Frosthaven",
+    "image": "character-perks/frosthaven/fh-trapper-perks.png",
+    "xws": "trapperperks"
   }
 ]
